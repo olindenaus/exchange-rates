0cdb48dbde69274012d2cc1da0d9009cbc251efa
Marcin <rhoninn11@gmail.com>
2019-05-19 14:31:01 +0200

    some block style changes and basic usage of exchange api

--- src/App.js
+++ src/App.js
@@ -4,14 +4,48 @@ import Layout from './hoc/Layout';
 import Block from './containers/Block/Block';
 import Button from './components/UI/Button/Button';
 import TransferPanel from './components/TransferPanel/TransferPanel';
+import Axios from 'axios';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cachedData: null
+    }
+
+  }
+
+  getData = () => {
+    Axios.get('https://api.exchangeratesapi.io/latest')
+      .then(result => {
+        this.setState({ cachedData: result.data })
+        console.log(result.data);
+      }).catch(error => {
+        console.log(error);
+      }).finally(() => {
+        //always execute
+      });
+  }
+
+  componentWillMount() {
+    this.getData();
+  }
+
   render() {
+
+    let currencies = null;
+    if (this.state.cachedData) {
+      currencies = (
+        <React.Fragment>
+          <Block currencies={this.state.cachedData.rates} />
+          <TransferPanel />
+          <Block currencies={this.state.cachedData.rates} />
+        </React.Fragment>
+      );
+    }
     return (
       <div className="App">
         <Layout>
-          <Block/>
-          <TransferPanel/>
-          <Block/>
+          {currencies}
         </Layout>
       </div>
     );
--- src/containers/Block/Block.js
+++ src/containers/Block/Block.js
@@ -4,12 +4,31 @@ import Input from '../../components/UI/Input/Input';
 import './Block.css';
 
 class Block extends Component {
-     render () {
-         return (
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount() {
+
+        let {currencies} = this.props;
+        if(currencies){
+            for(var rate in currencies){
+                console.log(rate, ':',currencies[rate]);
+            }
+        }
+        /* TODO: 
+            -sktóty walut umieścić w selektach
+            -on change selektów obsłóżyć
+            -doczyszenie kodu z Aoo odnośnie warunku podawania propsów do bloków
+        */ 
+    }
+
+    render() {
+        return (
             <div className="Block">
-                <Input label="CośtamLABEL" elementType="input">
-                </Input>
                 <Input label="Wybierz walutę:" elementType="select">
+                </Input>
+                <Input label="Wartość" elementType="input">
                 </Input>
             </div>
         );
